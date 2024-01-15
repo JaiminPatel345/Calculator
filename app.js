a0a76cecc873bd3845867c70ff7c23b9db0cb433
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2024-01-15 12:28:41 +0530

    error handling  for invalid syntax . configure +/- button .

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ let btns = document.querySelectorAll("button");
 
 let str="";
 let arr = Array.from(btns);
+let flag = false;
 
 arr.forEach(button => {
     button.addEventListener('click' , (e) => {
         if(e.target.innerHTML == '='){
-            str = eval(str);
-            input.value = str;
+            try{
+                str = eval(str);
+            }catch(error){
+                input.value = "Syntax Error";
+                input.style.color = "red";
+                flag = true;
+            }
         }
         else if(e.target.innerHTML == "AC"){
             str = "";
@@ -16,9 +22,17 @@ arr.forEach(button => {
         else if(e.target.innerHTML == "DEL"){
             str = str.slice(0,-1);
         }
+        else if(e.target.innerHTML == "+/-"){
+            if(str.charAt(0) != '-') str = "-".concat(str);
+            else str = str.substring(1);
+        }
         else{
             str+=e.target.innerHTML;
         }
-        input.value =str;
+        if(!flag){
+            input.value =str;
+            input.style.color = "white";
+        }
+        flag = false;
     })
 })
